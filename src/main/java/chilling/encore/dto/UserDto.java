536db85b68ff8c52e4cd272c7fc9f92a815dea42
ay-eonii <naayen@naver.com>
2023-04-16 22:53:43 +0900

    feat: 회원가입 구현

---
+++ src/main/java/chilling/encore/dto/UserDto.java
@@ -0,0 +1,25 @@
+package chilling.encore.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String name;
+    private String gender;
+    private int age;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String passwordCheck;
+    private String nickName;
+    private String phoneNumber;
+
+}
