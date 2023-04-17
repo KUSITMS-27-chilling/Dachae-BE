d8d7c5de492ffac986063ae6b1c49a1b521a988c
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-04-17 23:13:21 +0900

    fix: requestBody -> ModelAttribute

--- src/main/java/chilling/encore/controller/UserController.java
+++ src/main/java/chilling/encore/controller/UserController.java
@@ -10,10 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -38,31 +35,31 @@ public class UserController {
 
     @PostMapping("/checkIdDup")
     @ApiOperation(value = "아이디 중복체크", notes = "아이디 중복체크를 합니다.")
-    public ResponseEntity<ResponseDto<Boolean>> checkId(@RequestBody Map<String, String> id) {
+    public ResponseEntity<ResponseDto<Boolean>> checkId(@ModelAttribute String id) {
         return ResponseEntity.ok(
                 ResponseDto.create(
                         CHECK_DUP.getMessage(),
-                        userService.checkIdDup(id.get("id"))
+                        userService.checkIdDup(id)
                 )
         );
     }
 
     @PostMapping("/checkNickDup")
     @ApiOperation(value = "닉네임 중복체크", notes = "닉네임 중복체크를 합니다.")
-    public ResponseEntity<ResponseDto<Boolean>> checkNick(@RequestBody Map<String, String> nickName) {
+    public ResponseEntity<ResponseDto<Boolean>> checkNick(@ModelAttribute String nickName) {
         return ResponseEntity.ok(
                 ResponseDto.create(
                         CHECK_DUP.getMessage(),
-                        userService.checkNick(nickName.get("nickName"))
+                        userService.checkNick(nickName)
                 )
         );
     }
 
     @PostMapping("/sendMail")
     @ApiOperation(value = "이메일 인증 전송", notes = "이메일 인증을 전송합니다.")
-    public ResponseEntity<ResponseDto<String>> sendMail(@RequestBody Map<String, String> email) {
+    public ResponseEntity<ResponseDto<String>> sendMail(@ModelAttribute String email) {
         try {
-            String s = emailService.sendSimpleMessage(email.get("email"));
+            String s = emailService.sendSimpleMessage(email);
             return ResponseEntity.ok(ResponseDto.create(EMAIL_SEND_SUCCESS.getMessage(), s));
         } catch (Exception e) {
             e.printStackTrace();
