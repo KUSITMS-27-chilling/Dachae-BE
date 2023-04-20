dbb133fc22c28851c685ed7fb45ca0f997132c5e
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-04-21 05:02:26 +0900

    feat: 소셜로그인 컨트롤러 추가

---
+++ src/main/java/chilling/encore/controller/Oauth2Controller.java
@@ -0,0 +1,35 @@
+package chilling.encore.controller;
+
+import chilling.encore.global.dto.ResponseDto;
+import chilling.encore.service.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class Oauth2Controller {
+    private final SelectOauthService selectOauthService;
+
+    @GetMapping("/login/oauth2/code/{provider}")
+    public ResponseEntity<ResponseDto<?>> getUser(@PathVariable String provider, String code) {
+        provider = provider.toUpperCase();
+        Oauth2Service oauth2Service = selectOauthService.selectService(provider);
+        Oauth2AuthService oauth2AuthService = selectOauthService.selectAuth(provider);
+
+        String accessToken = oauth2AuthService.getAccessToken(code);
+        ResponseDto<?> authenticate = oauth2Service.authenticate(accessToken);
+
+        return ResponseEntity.ok(authenticate);
+    }
+
+    @GetMapping("/login/oauth2/{provider}")
+    public ResponseEntity<ResponseDto<?>> loginOAuth2Code(@PathVariable String provider,
+                                                          String token) {
+        Oauth2Service oauth2Service = selectOauthService.selectService(provider);
+        ResponseDto<?> authenticate = oauth2Service.authenticate(token);
+        return ResponseEntity.ok(authenticate);
+    }
+}
