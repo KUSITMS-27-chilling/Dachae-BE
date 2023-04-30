227508245104db582c744f9af5ef877c0937622c
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-01 02:42:35 +0900

    feat: Center 테이블 추가

---
+++ src/main/java/chilling/encore/domain/Center.java
@@ -0,0 +1,23 @@
+package chilling.encore.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class Center {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long regionIdx;
+
+    private String region;
+    private int favCount;
+    private String tell;
+}
