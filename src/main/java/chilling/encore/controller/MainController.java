f4665aa8197db6414029dcf78445711b2e245718
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-01 02:53:19 +0900

    fix: MainController 첫페이지 전체 조회(로그인X) 구현

---
+++ src/main/java/chilling/encore/controller/MainController.java
@@ -0,0 +1,32 @@
+package chilling.encore.controller;
+
+import chilling.encore.global.dto.ResponseDto;
+import chilling.encore.service.MainService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static chilling.encore.dto.responseMessage.MainConstants.SuccessMessage.SELECT_SUCCESS;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/main")
+@Api(tags = "MainPage API")
+public class MainController {
+    private final MainService mainService;
+
+    @GetMapping()
+    @ApiOperation(value = "메인페이지 접근", notes = "필요한 모든 데이터 전송")
+    public ResponseEntity<ResponseDto<Map<String, Object>>> getFirstPage() {
+        Map<String, Object> firstPage = mainService.getFirstPage();
+        return ResponseEntity.ok(ResponseDto.create(SELECT_SUCCESS.getMessage(), firstPage));
+    }
+}
