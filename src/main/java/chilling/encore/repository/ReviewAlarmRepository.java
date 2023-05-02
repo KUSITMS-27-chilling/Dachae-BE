8833640d8af0ee3f7eedaae68c453fe9beabbeb3
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-02 22:59:32 +0900

    feat: 알람 조회

--- src/main/java/chilling/encore/repository/ReviewAlarmRepository.java
+++ src/main/java/chilling/encore/repository/ReviewAlarmRepository.java
@@ -3,5 +3,8 @@ package chilling.encore.repository;
 import chilling.encore.domain.ReviewAlarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewAlarmRepository extends JpaRepository<ReviewAlarm, Long> {
+    List<ReviewAlarm> findAllByIsReadFalseAndUser_UserIdx(Long userIdx);
 }
