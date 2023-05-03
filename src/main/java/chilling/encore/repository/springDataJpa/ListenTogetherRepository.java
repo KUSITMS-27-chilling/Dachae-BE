c0f876ca69ff3e048b644eaeb3566fd912665061
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-03 19:47:59 +0900

    feat: QueryDsl ListenTogetherDslRepository 생성

--- src/main/java/chilling/encore/repository/springDataJpa/ListenTogetherRepository.java
+++ src/main/java/chilling/encore/repository/springDataJpa/ListenTogetherRepository.java
@@ -1,11 +1,12 @@
 package chilling.encore.repository.springDataJpa;
 
 import chilling.encore.domain.ListenTogether;
+import chilling.encore.repository.querydsl.ListenTogetherDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ListenTogetherRepository extends JpaRepository<ListenTogether, Long> {
+public interface ListenTogetherRepository extends JpaRepository<ListenTogether, Long>, ListenTogetherDslRepository {
 //    ListenTogether findTopByOrderByHitsDesc(String region);
     List<ListenTogether> findTopByOrderByHitDesc();
 
