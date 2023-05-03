45ca12bd2d032e1ca3e57f54e14c07114c8020e8
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-03 17:15:48 +0900

    feat: 상세보기 1차 완료

--- src/main/java/chilling/encore/controller/MainController.java
+++ src/main/java/chilling/encore/controller/MainController.java
@@ -1,22 +1,24 @@
 package chilling.encore.controller;
 
+import chilling.encore.dto.MainDto;
+import chilling.encore.dto.MainDto.DetailResponse;
 import chilling.encore.dto.MainDto.MainResponse;
 import chilling.encore.dto.ProgramDto;
-import chilling.encore.dto.ProgramDto.NewProgram;
 import chilling.encore.dto.ProgramDto.NewProgramsResponse;
 import chilling.encore.global.dto.ResponseDto;
+import chilling.encore.service.DetailsService;
 import chilling.encore.service.MainService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 import static chilling.encore.dto.responseMessage.MainConstants.SuccessMessage.*;
 
@@ -27,6 +29,7 @@ import static chilling.encore.dto.responseMessage.MainConstants.SuccessMessage.*
 @Api(tags = "MainPage API")
 public class MainController {
     private final MainService mainService;
+    private final DetailsService detailsService;
 
     @GetMapping()
     @ApiOperation(value = "메인페이지 접근", notes = "필요한 모든 데이터 전송")
@@ -40,5 +43,12 @@ public class MainController {
     public ResponseEntity<ResponseDto<NewProgramsResponse >> getNewPrograms(@PathVariable String region) {
         NewProgramsResponse newPrograms = mainService.getNewPrograms(region);
         return ResponseEntity.ok(ResponseDto.create(CHANGE_NEWPROGRAM_SUCCESS.getMessage(), newPrograms));
+    }
+
+    @GetMapping(value = {"/details/{region}/{page}", "/details/{region}"})
+    @ApiOperation(value = "모아보기의 상세보기", notes = "page는 첫 진입시 안보내줘도 괜춘 /details/{region}")
+    public ResponseEntity<ResponseDto<DetailResponse>> getDetails(@PathVariable String region, @PathVariable @Nullable Integer page) {
+        DetailResponse details = detailsService.getDetails(region, page);
+        return ResponseEntity.ok(ResponseDto.create("", details));
     }
 }
