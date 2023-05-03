e109a70b138d446e7655a4aa7f6cd8f80fc47a72
ay-eonii <naayen@naver.com>
2023-05-03 15:24:36 +0900

    feat: 같이들어요 게시글 조회 기능 임시 추가

--- src/main/java/chilling/encore/repository/ListenTogetherRepository.java
+++ src/main/java/chilling/encore/repository/ListenTogetherRepository.java
@@ -3,5 +3,14 @@ package chilling.encore.repository;
 import chilling.encore.domain.ListenTogether;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ListenTogetherRepository extends JpaRepository<ListenTogether, Long> {
+//    ListenTogether findTopByOrderByHitsDesc(String region);
+    List<ListenTogether> findTopByOrderByHitDesc();
+
+//    ListenTogether findAllByRegion(String region);
+//    List<ListenTogether> findAllByRegion();
+
+//    ListenTogether findByRegion(String region);
 }
