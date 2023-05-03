d4bdb99e58e646bd6886f65071d1ff2d2be18e04
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-03 22:28:35 +0900

    feat: 상세보기 DTO 작성

--- src/main/java/chilling/encore/dto/MainDto.java
+++ src/main/java/chilling/encore/dto/MainDto.java
@@ -1,6 +1,5 @@
 package chilling.encore.dto;
 
-import chilling.encore.dto.ProgramDto.GetDetailPrograms;
 import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,29 +27,6 @@ public abstract class MainDto {
                     .newPrograms(newPrograms)
                     .grade(grade)
                     .regionPrograms(regionPrograms)
-                    .build();
-        }
-    }
-
-    @Getter
-    @Builder
-    @ApiModel(description = "모아보기의 상세보기 접근을 위한 응답객체")
-    public static class DetailResponse {
-        private int favCount;
-        private String tell;
-        private List<GetDetailPrograms> programs;
-        /**
-         * 같이들어요 List 추가
-         */
-        public static DetailResponse from(
-                int favCount,
-                String tell,
-                List<GetDetailPrograms> programs
-        ) {
-            return DetailResponse.builder()
-                    .favCount(favCount)
-                    .tell(tell)
-                    .programs(programs)
                     .build();
         }
     }
