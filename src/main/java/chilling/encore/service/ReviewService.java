4e2aab415eb4e4b3b3ab3ecdb8b386f70be9b832
ay-eonii <naayen@naver.com>
2023-05-08 02:14:27 +0900

    fix: 수강후기 인기글 조회 수정

--- src/main/java/chilling/encore/service/ReviewService.java
+++ src/main/java/chilling/encore/service/ReviewService.java
@@ -13,8 +13,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.sound.sampled.ReverbType;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -55,11 +56,10 @@ public class ReviewService {
 
     public List<PopularReview> getPopularReview(String region) {
         String[] regions = region.split(",");
-//        List<Review> Reviews = new ArrayList<>();
-//        for (String r : regions) {
-//            Reviews.add(reviewRepository.findAllByProgram_LearningCenter_RegionByHitDesc(r));
-//        }
         List<Review> Reviews = reviewRepository.findRegionReview(regions);
+        Comparator<Review> hitComparator = Comparator.comparingInt(Review::getHit).reversed();
+        Collections.sort(Reviews, hitComparator);
+
         List<PopularReview> popularReviews = new ArrayList<>();
         for (Review review : Reviews) {
             popularReviews.add(PopularReview.from(review));
@@ -67,6 +67,7 @@ public class ReviewService {
                 break;
             }
         }
+
         return popularReviews;
     }
 }
