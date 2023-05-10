939674cf6691f2575c043445a136c31a7b9bc1e9
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-10 15:51:20 +0900

    feat: 같이할래요 신청 관련 예외 처리 구현

--- src/main/java/chilling/encore/dto/responseMessage/ParticipantConstants.java
+++ src/main/java/chilling/encore/dto/responseMessage/ParticipantConstants.java
@@ -10,4 +10,24 @@ public class ParticipantConstants {
         CREATE_SUCCESS_MESSAGE("신청에 성공했습니다.");
         private final String message;
     }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum FailMessage {
+        DUPLICATED_MESSAGE("[ERROR] 이미 신청한 게시글입니다."),
+        MINE_MESSAGE("[ERROR] 본인이 작성한 게시글입니다."),
+        FULL_MESSAGE("[ERROR] 신청 인원이 가득 찼습니다.");
+
+        private final String message;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum FailCode {
+        DUPLICATED_CODE("pa001"),
+        MINE_CODE("pa002"),
+        FULL_CODE("pa003");
+
+        private final String code;
+    }
 }
--- src/main/java/chilling/encore/service/ParticipantService.java
+++ src/main/java/chilling/encore/service/ParticipantService.java
@@ -4,10 +4,15 @@ import chilling.encore.domain.ListenTogether;
 import chilling.encore.domain.Participants;
 import chilling.encore.domain.User;
 import chilling.encore.dto.ParticipantDto;
+import chilling.encore.exception.ParticipantException;
+import chilling.encore.exception.ParticipantException.DuplicateParticipationException;
+import chilling.encore.exception.ParticipantException.FullParticipantException;
+import chilling.encore.exception.ParticipantException.MyListenTogetherException;
 import chilling.encore.global.config.security.util.SecurityUtils;
 import chilling.encore.repository.springDataJpa.ListenTogetherRepository;
 import chilling.encore.repository.springDataJpa.ParticipantsRepository;
 import chilling.encore.repository.springDataJpa.UserRepository;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -28,18 +33,21 @@ public class ParticipantService {
                         .getUserIdx()).orElseThrow();
         log.info("listenTogetherIdx = {}", participantRequest.getListenTogetherIdx());
         ListenTogether listenTogether = listenTogetherRepository.findById(participantRequest.getListenTogetherIdx()).orElseThrow();
-        if (listenTogether.getParticipants().size() >= listenTogether.getGoalNum())
-            throw new IllegalArgumentException("더이상 불가능 (나중에 제대로 예외 처리)");
-
-        if (participantsRepository.existsByUserAndListenTogether(user, listenTogether))
-            throw new IllegalArgumentException();
-        if (listenTogether.getUser().getUserIdx() == user.getUserIdx())
-            throw new IllegalArgumentException();
+        validate(user, listenTogether);
 
         Participants participants = Participants.builder()
                 .listenTogether(listenTogether)
                 .user(user)
                 .build();
         participantsRepository.save(participants);
+    }
+
+    private void validate(User user, ListenTogether listenTogether) {
+        if (listenTogether.getParticipants().size() >= listenTogether.getGoalNum())
+            throw new FullParticipantException();
+        if (participantsRepository.existsByUserAndListenTogether(user, listenTogether))
+            throw new DuplicateParticipationException();
+        if (listenTogether.getUser().getUserIdx() == user.getUserIdx())
+            throw new MyListenTogetherException();
     }
 }
