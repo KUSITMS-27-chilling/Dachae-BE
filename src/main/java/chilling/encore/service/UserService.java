11e527616c23fb0ddb979b608aec13d8bd73dedd
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-10 22:24:55 +0900

    feat: 자유게시판 Entity 생성

--- src/main/java/chilling/encore/service/UserService.java
+++ src/main/java/chilling/encore/service/UserService.java
@@ -2,6 +2,8 @@ package chilling.encore.service;
 
 import chilling.encore.domain.Center;
 import chilling.encore.domain.User;
+import chilling.encore.dto.AlaramDto;
+import chilling.encore.dto.AlaramDto.AlarmResponse;
 import chilling.encore.dto.UserDto.UserLoginRequest;
 import chilling.encore.dto.UserDto.UserLoginResponse;
 import chilling.encore.dto.UserDto.UserSignUpRequest;
@@ -23,9 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static chilling.encore.dto.UserDto.*;
 import static chilling.encore.dto.responseMessage.UserConstants.Role.ROLE_USER;
@@ -217,5 +217,34 @@ public class UserService {
         int allTotal = listenTotal + reviewTotal;
 
         return GetTotalWrite.from(allTotal, listenTotal, reviewTotal);
+    }
+
+    public AlarmResponse getAlarm() {
+        User user = SecurityUtils.getLoggedInUser().orElseThrow(() -> new ClassCastException("NotLogin"));
+        Set<String> notificationIds = redisRepository.getNotificationIds(String.valueOf(user.getUserIdx()), 0, -1);
+        List<String> notifications = redisRepository.getNotifications(String.valueOf(user.getUserIdx()), notificationIds);
+        Iterator<String> notificationIdIterator = notificationIds.iterator();
+        List<AlaramDto.NewAlarm> newAlarms = getNewAlarms(notifications, notificationIdIterator);
+
+        return AlarmResponse.from(newAlarms);
+    }
+
+    private List<AlaramDto.NewAlarm> getNewAlarms(List<String> notifications, Iterator<String> notificationIdIterator) {
+        List<AlaramDto.NewAlarm> newAlarms = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            String[] splitDatas = notifications.get(i).split(":");
+            String boardType = splitDatas[0];
+            String title = splitDatas[2];
+            String nickName = splitDatas[3];
+            String content = splitDatas[4];
+            if (boardType.equals("Listen")) {
+                newAlarms.add(AlaramDto.NewAlarm.from(notificationIdIterator.next(), Long.parseLong(splitDatas[1]),
+                        null, title, nickName, content));
+                continue;
+            }
+            newAlarms.add(AlaramDto.NewAlarm.from(notificationIdIterator.next(), null, Long.parseLong(splitDatas[1]),
+                    title, nickName, content));
+        }
+        return newAlarms;
     }
 }
