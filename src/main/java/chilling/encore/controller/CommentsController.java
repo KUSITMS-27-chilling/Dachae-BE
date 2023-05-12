d5f4e8aaeb62cdc7abeaabaceb878bda4a5410ee
ay-eonii <naayen@naver.com>
2023-05-13 03:17:43 +0900

    feat: 같이들어요 댓글 기능 추가
    수강후기 댓글과 같이들어요 댓글 Dto는 CommentsDto 공유

--- src/main/java/chilling/encore/controller/CommentsController.java
+++ src/main/java/chilling/encore/controller/CommentsController.java
@@ -1,6 +1,6 @@
 package chilling.encore.controller;
 
-import chilling.encore.dto.ReviewCommentsDto.CreateReviewCommentsRequest;
+import chilling.encore.dto.ReviewCommentsDto.CreateCommentsRequest;
 import chilling.encore.global.dto.ResponseDto;
 import chilling.encore.service.CommentsService;
 import io.swagger.annotations.Api;
@@ -8,11 +8,13 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static chilling.encore.dto.responseMessage.ReviewConstants.ReviewSuccessMessage.CREATE_SUCCESS_MESSAGE;
+import static chilling.encore.dto.responseMessage.ListenTogetherConstants.ListenTogetherSuccessMessage.*;
+import static chilling.encore.dto.responseMessage.ReviewConstants.ReviewSuccessMessage.*;
 import static chilling.encore.global.dto.ResponseCode.globalSuccessCode.CREATE_SUCCESS_CODE;
 
 @RestController
@@ -22,9 +24,21 @@ import static chilling.encore.global.dto.ResponseCode.globalSuccessCode.CREATE_S
 public class CommentsController {
     private final CommentsService commentsService;
     @PostMapping("/review/{reviewIdx}/comments")
-    @ApiOperation(value = "수강후기 댓글 작성", notes = "reviewIdx, ref, refOrder, step 함께 넘겨주세요")
-    public ResponseEntity<ResponseDto> save(@RequestBody CreateReviewCommentsRequest createReviewCommentsRequest) {
-        commentsService.save(createReviewCommentsRequest);
-        return ResponseEntity.ok(ResponseDto.create(CREATE_SUCCESS_CODE.getCode(), CREATE_SUCCESS_MESSAGE.getMessage()));
+    @ApiOperation(value = "수강후기 댓글 작성", notes = "ref, refOrder, step 함께 넘겨주세요")
+    public ResponseEntity<ResponseDto> ReviewCommentSave(
+            @PathVariable Long reviewIdx,
+            @RequestBody CreateCommentsRequest createCommentsRequest) {
+        commentsService.reviewCommentSave(reviewIdx, createCommentsRequest);
+        return ResponseEntity.ok(ResponseDto.create(CREATE_SUCCESS_CODE.getCode(), CREATE_REVIEW_COMMENT_SUCCESS_MESSAGE.getMessage()));
     }
+
+    @PostMapping("/listen/{listenIdx}/comments")
+    @ApiOperation(value = "같이들어요 댓글 작성", notes = "ref, refOrder, step 함께 넘겨주세요")
+    public ResponseEntity<ResponseDto> ListenCommentSave(
+            @PathVariable Long listenIdx,
+            @RequestBody CreateCommentsRequest createCommentsRequest) {
+        commentsService.listenCommentSave(listenIdx, createCommentsRequest);
+        return ResponseEntity.ok(ResponseDto.create(CREATE_SUCCESS_CODE.getCode(), CREATE_LISTEN_COMMENT_SUCCESS_MESSAGE.getMessage()));
+    }
+
 }
--- src/main/java/chilling/encore/dto/CommentsDto.java
+++ src/main/java/chilling/encore/dto/CommentsDto.java
@@ -1,19 +1,14 @@
 package chilling.encore.dto;
 
-import chilling.encore.domain.Review;
 import io.swagger.annotations.ApiModel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 public abstract class ReviewCommentsDto {
     @Getter
     @RequiredArgsConstructor
-    @ApiModel(description = "수강후기 댓글을 위한 요청 객체")
-    public static class CreateReviewCommentsRequest {
-        Long reviewIdx;
+    @ApiModel(description = "댓글을 위한 요청 객체")
+    public static class CreateCommentsRequest {
         int ref;
         int refOrder;
         int step;
