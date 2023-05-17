1d9eec6de2972f5e98ef08a1667b9c6b87351d4e
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-05-17 22:16:38 +0900

    feat: 후기 예외 처리

---
+++ src/main/java/chilling/encore/exception/ReviewException.java
@@ -0,0 +1,20 @@
+package chilling.encore.exception;
+
+import chilling.encore.dto.responseMessage.ReviewConstants;
+import chilling.encore.dto.responseMessage.ReviewConstants.ReviewFailCode;
+import chilling.encore.dto.responseMessage.ReviewConstants.ReviewFailMessage;
+import chilling.encore.global.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class ReviewException extends ApplicationException {
+    protected ReviewException(String message, String errorCode, HttpStatus httpStatus) {
+        super(message, errorCode, httpStatus);
+    }
+
+    public static class NoSuchIdxException extends ReviewException{
+
+        public NoSuchIdxException() {
+            super(ReviewFailMessage.NO_SUCH_IDX_MESSAGE.getMessage(), ReviewFailCode.NO_SUCH_IDX_MESSAGE.getCode(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
