b10343f6e69fc4c591670de2057fd4f0a1226fd6
yang <ywj9811@gmail.com>
2023-06-15 21:01:03 +0900

    refactor: 슬랙 푸시 추가 및 jwtException 처리 중복 코드 제거

--- src/main/java/chilling/encore/global/config/security/filter/CustomAccessDeniedHandler.java
+++ src/main/java/chilling/encore/global/config/security/filter/CustomAccessDeniedHandler.java
@@ -1,6 +1,7 @@
 package chilling.encore.global.config.security.filter;
 
 import chilling.encore.global.config.security.exception.SecurityException.AuthorityException;
+import chilling.encore.global.config.slack.SlackMessage;
 import chilling.encore.global.dto.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,7 @@ import static chilling.encore.global.config.security.jwt.JwtConstants.JwtExcpeti
 @RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private final ObjectMapper objectMapper;
+    private final SlackMessage slackMessage;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
@@ -32,10 +34,10 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
                 JwtExcpetionMessage.NON_AUTHORITY.getMessage(),
                 JwtExcpetionCode.NON_AUTHORITY.getCode(),
                 HttpStatus.FORBIDDEN);
+        slackMessage.sendSlackAlertErrorLog(e, JwtExcpetionCode.NON_AUTHORITY.getCode(), request);
         response.setStatus(e.getHttpStatus().value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
-
         String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
         response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
     }
--- src/main/java/chilling/encore/global/config/security/filter/JwtExceptionFilter.java
+++ src/main/java/chilling/encore/global/config/security/filter/JwtExceptionFilter.java
@@ -2,7 +2,9 @@ package chilling.encore.global.config.security.filter;
 
 import chilling.encore.global.config.security.exception.SecurityException;
 import chilling.encore.global.config.security.exception.SecurityException.*;
+import chilling.encore.global.config.slack.SlackMessage;
 import chilling.encore.global.dto.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -19,49 +21,40 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtExceptionFilter extends OncePerRequestFilter {
     private final ObjectMapper objectMapper;
+    private final SlackMessage slackMessage;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try{
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException e) {
-            response.setStatus(e.getHttpStatus().value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-
-            String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+            String errorResponseJson = getErrorResponseJson(request, response, e);
             response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
 
         } catch (InvalidJwtFormatException e) {
-            response.setStatus(e.getHttpStatus().value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-
-            String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+            String errorResponseJson = getErrorResponseJson(request, response, e);
             response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
 
         } catch (NonSupportedJwtException e) {
-            response.setStatus(e.getHttpStatus().value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-
-            String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+            String errorResponseJson = getErrorResponseJson(request, response, e);
             response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
 
         } catch (WrongTokenException e) {
-            response.setStatus(e.getHttpStatus().value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-
-            String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+            String errorResponseJson = getErrorResponseJson(request, response, e);
             response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
 
         } catch (UnKnownException e) {
-            response.setStatus(e.getHttpStatus().value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-
-            String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+            String errorResponseJson = getErrorResponseJson(request, response, e);
             response.getOutputStream().write(errorResponseJson.getBytes("UTF-8"));
         }
+    }
+
+    private String getErrorResponseJson(HttpServletRequest request, HttpServletResponse response, SecurityException e) throws JsonProcessingException {
+        response.setStatus(e.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        slackMessage.sendSlackAlertErrorLog(e, e.getErrorCode(), request);
+        String errorResponseJson = objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+        return errorResponseJson;
     }
 }
