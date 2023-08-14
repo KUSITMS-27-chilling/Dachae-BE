a84552513ba6e3082a92425984835106b5dc865b
yang <ywj9811@gmail.com>
2023-08-15 03:56:26 +0900

    fix: test lombok dependency 추가

--- src/test/java/chilling/encore/utils/domain/MockListenTogether.java
+++ src/test/java/chilling/encore/utils/domain/MockListenTogether.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class MockListenTogether extends ListenTogether {
     public MockListenTogether(Long listenIdx, User user, Program program) {
         this.listenIdx = listenIdx;
@@ -28,103 +29,4 @@ public class MockListenTogether extends ListenTogether {
     private LocalDateTime updatedAt = LocalDateTime.now();
     List<ListenComments> listexnComments = new ArrayList<>();
     List<Participants> participants = new ArrayList<>();
-
-    public void setListenIdx(Long listenIdx) {
-        this.listenIdx = listenIdx;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public void setProgram(Program program) {
-        this.program = program;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setHit(int hit) {
-        this.hit = hit;
-    }
-
-    public void setGoalNum(int goalNum) {
-        this.goalNum = goalNum;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public void setListexnComments(List<ListenComments> listexnComments) {
-        this.listexnComments = listexnComments;
-    }
-
-    public void setParticipants(List<Participants> participants) {
-        this.participants = participants;
-    }
-
-    @Override
-    public Long getListenIdx() {
-        return listenIdx;
-    }
-
-    @Override
-    public User getUser() {
-        return user;
-    }
-
-    @Override
-    public Program getProgram() {
-        return program;
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public String getContent() {
-        return content;
-    }
-
-    @Override
-    public int getHit() {
-        return hit;
-    }
-
-    @Override
-    public int getGoalNum() {
-        return goalNum;
-    }
-
-    @Override
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    @Override
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    @Override
-    public List<ListenComments> getListexnComments() {
-        return listexnComments;
-    }
-
-    @Override
-    public List<Participants> getParticipants() {
-        return participants;
-    }
 }
